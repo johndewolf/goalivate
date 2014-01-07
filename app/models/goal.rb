3373498a9d87330a5a831ed80caada2b486b45b7
johndewolf <jdewolf06@gmail.com>
2014-01-07 15:05:19 -0500

    all specs past

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,8 +3,10 @@ class Goal < ActiveRecord::Base
   validates_presence_of :starting_strength
   validates_presence_of :goal_weight
   validates_presence_of :goal_date
-  validate :date_is_in_the_future
-  validate :goal_is_greater_than_start
+  validate :date_is_in_the_future,
+    if: -> (goal) { goal.goal_date.present? }
+  validate :goal_is_greater_than_start,
+    if: -> (goal) { goal.goal_weight.present? && goal.starting_strength.present? }
   belongs_to :user,
   inverse_of: :goals
 
@@ -16,13 +18,13 @@ class Goal < ActiveRecord::Base
 
 
   def goal_is_greater_than_start
-    unless goal_weight.present? && starting_strength.present? && goal_weight > starting_strength
+    unless goal_weight > starting_strength
       errors[:goal_weight] << 'Goal Weight must be greater than starting strength'
     end
   end
 
   def date_is_in_the_future
-    unless goal_date.present? && goal_date > Date.today + 7
+    unless goal_date > Date.today + 7
       errors[:goal_date] << "must be 7 days in the future"
     end
   end
