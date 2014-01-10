8e1ef1ce3274ef37e03feeb28761285292990536
johndewolf <jdewolf06@gmail.com>
2014-01-10 15:21:58 -0500

    added csv to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+datafile = Rails.root + 'db/data/exercises.csv'
+
+CSV.foreach(datafile, headers: true) do |row|
+  Exercise.find_or_create_by(name: row['name'])
+end
