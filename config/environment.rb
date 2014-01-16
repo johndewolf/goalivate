1812687d1a886ff6486edd8a8a9fc2ddecc6aab1
johndewolf <jdewolf06@gmail.com>
2014-01-16 13:56:50 -0500

    added config for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 BreakableToyTwo::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app21279338@heroku.com',
+  :password => '',
+  :domain => 'http://goalivate.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
