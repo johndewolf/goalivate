d9793b14ec1e0b661f84f92809cb4a050d534399
johndewolf <jdewolf06@gmail.com>
2014-01-22 12:19:50 -0500

    added quotes to index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def random_quotes
+    quotes = [
+      "You are never too old to set another goal or to dream a new dream. -C.S. Lewise",
+      "The best revenge is massive success. -Frank Sinatra",
+      "A goal is a dream with a deadline. -Napoleon Hill" ]
+
+    quotes.sample
+  end
 end
